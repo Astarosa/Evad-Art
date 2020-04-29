c8dc98296564419f9ec67c8491af6d3eda56f234
Astarosa <baptiste.shortgibet@gmail.com>
2020-04-29 13:09:53 +0200

    add Banner files

---
+++ src/Components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = () => {
+    return(
+        <header>
+            <h1></h1>
+            <h2></h2>
+        </header>
+    )
+}
+
+export default Banner;
