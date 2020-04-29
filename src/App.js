de6e7514bfe1764b0747d3a581a99bd36b4ebf53
Astarosa <baptiste.shortgibet@gmail.com>
2020-04-29 16:46:30 +0200

    getbackgroundimage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import './Styles/App.css';
+import Header from './Components/Header';
 import Home from './Pages/Home';
+import axios from 'axios';
 
-function App () {
-  return (
-    <div className='App'>
-      <Home />
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      ids: []
+    };
+  }
+
+  getBackGroundImage = () => {
+    axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=9')
+      .then(res => res.data)
+      .then(
+        (data) => {
+          console.log(data.objectIDs);
+          console.log(Object.keys(data));
+          this.setState({
+            isLoaded: true,
+            ids: data.objectIDs
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  componentDidMount () {
+    return this.getBackGroundImage();
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <Header />
+        <Home />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className='banner-background'>
+      <h1>EVAD'ART</h1>
+      <h2>- Comprendre l'art à travers les époques -</h2>
+    </header>
+  );
+};
+
+export default Header;
