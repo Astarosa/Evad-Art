6ecad16a471ffd400a5fce649298cf62d1875027
Astarosa <baptiste.shortgibet@gmail.com>
2020-04-29 14:09:18 +0200

    fix dependencies dans standardJS

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const Banner = () => {
-    return(
-        <header>
-            <h1></h1>
-            <h2></h2>
-        </header>
-    )
+  return (
+    <header>
+      <h1>Title</h1>
+      <h2>under-tittle</h2>
+    </header>
+  )
 }
 
-export default Banner;
+export default Banner
