768e084105d8ccdf0027f19a03c6808e89c607c4
Amélie <ameliechevalier3006@gmail.com>
2020-04-30 12:59:05 +0200

    progression bar below banner and final version for demo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import '../Styles/Header.css';
 
 const Header = (props) => {
   return (
-    <header className='banner-background' style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(${props.backgroundImageUrl})` }}>
+    <header className={props.isLoaded ? 'banner-background' : 'banner-background wait'} style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(${props.backgroundImageUrl})` }}>
       <h1>EVAD'ART</h1>
       <h2>- Comprendre l'art au fil des époques -</h2>
     </header>
